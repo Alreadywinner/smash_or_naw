63876efde2fd1bebf2d346cae62ff4b4b5e717cc
Waseel Fawad <waseelfawad@gmail.com>
2023-05-16 17:39:39 +0500

    feat(Routes): pages routes setup by using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./pages/HomePage/HomePage";
+import { Routes, Route } from "react-router-dom";
+import RatingPage from "./pages/RatingPage/RatingPage";
 
 function App() {
   return (
     <>
       <NavBar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rating" element={<RatingPage />} />
+      </Routes>
       <Footer />
     </>
   );

5eabf2ff5656329943226a9645a86b4e265eba21
Waseel Fawad <waseelfawad@gmail.com>
2023-05-16 17:40:12 +0500

    feat(Ad): Ad component added in Rating Page

---
+++ src/components/Ad/Ad.jsx
@@ -0,0 +1,13 @@
+const Ad = () => {
+  return (
+    <div className="h-full">
+      <img
+        src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+        alt="..."
+        className="h-full object-cover rounded"
+      />
+    </div>
+  );
+};
+
+export default Ad;

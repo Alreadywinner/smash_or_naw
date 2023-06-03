beb5ed8d68753b51b309f284f08322b6669fbc04
Waseel Fawad <waseelfawad@gmail.com>
2023-06-03 05:09:59 +0500

    backend login signup complete now Integration is in progress

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 app.get("/", (req, res) => res.json({ message: "Hello World" }));

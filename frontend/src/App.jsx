f133db085a175bac714461b6c060027ea6d5530c
Waseel Fawad <waseelfawad@gmail.com>
2023-06-21 03:04:29 +0500

    revert the changes of ignore and added some new routes for admin side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
-import Footer from "./components/Footer/Footer";
-import NavBar from "./components/NavBar/NavBar";
-import TermsAndConditions from "./components/TermsAndConditions/TermsAndConditions";
-import HomePage from "./pages/HomePage/HomePage";
-import { Routes, Route } from "react-router-dom";
-import RatingPage from "./pages/RatingPage/RatingPage";
-import ContactUs from "./pages/ContactUs/ContactUs";
+import Footer from './components/Footer/Footer';
+import NavBar from './components/NavBar/NavBar';
+import TermsAndConditions from './components/TermsAndConditions/TermsAndConditions';
+import HomePage from './pages/HomePage/HomePage';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import RatingPage from './pages/RatingPage/RatingPage';
+import ContactUs from './pages/ContactUs/ContactUs';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import AdminPage from './pages/AdminPage/AdminPage';
+import AdPage from './pages/AdPage/AdPage';
 
 function App() {
+  const location = useLocation();
+  const { pathname } = location;
+  const isRouteAdmin = pathname.includes('admin');
+
   return (
     <>
-      <NavBar />
+      {!isRouteAdmin && <NavBar />}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/rating" element={<RatingPage />} />
+        <Route path="/admin/login" element={<AdminPage />} />
+        <Route path="/admin/Ad" element={<AdPage />} />
+        <Route path="" element={<PrivateRoute />}>
+          <Route path="/rating" element={<RatingPage />} />
+        </Route>
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
-      <Footer />
+      {!isRouteAdmin && <Footer />}
     </>
   );
 }
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,20 @@
+import { getStorage } from 'firebase/storage';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUyr73f-T56Z2DmjOXMc4j5uKAOVCYJX8",
-  authDomain: "smash-cc216.firebaseapp.com",
-  projectId: "smash-cc216",
-  storageBucket: "smash-cc216.appspot.com",
-  messagingSenderId: "1081798528584",
-  appId: "1:1081798528584:web:4e3feffa6dbcd68f30527d",
+  apiKey: 'AIzaSyCUyr73f-T56Z2DmjOXMc4j5uKAOVCYJX8',
+  authDomain: 'smash-cc216.firebaseapp.com',
+  projectId: 'smash-cc216',
+  storageBucket: 'smash-cc216.appspot.com',
+  messagingSenderId: '1081798528584',
+  appId: '1:1081798528584:web:4e3feffa6dbcd68f30527d',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage();

5909e8fe3fc608271cb0ecff98aab89703bacb96
Waseel Fawad <waseelfawad@gmail.com>
2023-07-01 17:54:51 +0500

    helper for changing time string to locale

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,3 +1,14 @@
 export function findUniqueValues(array1, array2) {
   return array1.filter((element) => !array2.includes(element));
 }
+
+export function convertToLocalAM_PM(dateString) {
+  const date = new Date(dateString);
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+
+  return date.toLocaleString('en-US', options);
+}
